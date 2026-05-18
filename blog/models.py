f778569e78686e271360eead9745ffe0c2bd1dc4
QuiD-10 <gduon008@ucr.edu>
2026-05-18 16:48:39 -0700

    Added Bootstrap and created Home Page with Jobs

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+#title
+#publish data
+#body
+#image
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pubDate = models.DateTimeField()
+    image = models.ImageField(upload_to='images/')
+    body = models.TextField(max_length=3000)
+
+
+#add to the admin
