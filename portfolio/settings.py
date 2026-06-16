c7c3a77eff8e303522d67a5d1aa3745fa63a2a5d
QuiD-10 <gduon008@ucr.edu>
2026-06-16 15:12:14 -0700

    testing allowed host

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -27,11 +27,7 @@ SECRET_KEY = 'django-insecure-c0u%74%nd$i#^6&q^wt9*yl*4=0i-n1s^*@egdi5$hz$&rg(hp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    '.vercel.app',
-    'localhost', 
-    '127.0.0.1'
-]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
